e886fff4c07741310e4a2945f7c543bfe7128235
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-01 21:52:39 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,1 +1,13 @@
-console.log(name)
+var index = require("./index.js")
+
+
+if (index.name === "Joe") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
+if (index.height === "70") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
